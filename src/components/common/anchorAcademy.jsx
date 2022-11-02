bbf7bb20deb2bdeda21a4d83d5df4dd332e2d84d
Karla Abad <kabad1014@gmail.com>
2022-11-02 09:56:06 -0400

    Update anchor academy tab.

--- src/components/common/anchorAcademy.jsx
+++ src/components/common/anchorAcademy.jsx
@@ -2,8 +2,8 @@ import anchorAcademy from "../../images/anchorAcademy.jpg";
 const AnchorAcademy = () => {
   return (
     <article>
-      <a href="https://test-1-9657d.web.app/">
-        <img src={anchorAcademy} alt="Project Image" target="_blank" />
+      <a href="https://test-1-9657d.web.app/" target="_blank">
+        <img src={anchorAcademy} alt="Project Image" />
         <div className="article__content">
           <h1>School Database</h1>
           <p>
